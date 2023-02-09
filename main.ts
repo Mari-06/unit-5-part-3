e70067df674e7cc95399f96f2bc7231b520c81b7
Mr-Harris <124732072+Mr-Harris@users.noreply.github.com>
2023-02-09 14:07:23 -0600

    Password creation

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+let password = game.askForString("WHAT IS THE PASSWORD?")
+if (password == "Pizzas" || password == "Never Gonna Give You Up!") {
+    game.splash("LOGIN SUCCESSFUL!")
+} else {
+    game.splash("LOGIN FAILURE!")
+}
